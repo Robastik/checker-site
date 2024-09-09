768917de24e55c8f82474df06b63812b6dd3d23a
Denis Mamaev <expertykt@mail.ru>
2024-09-09 12:03:04 +0900

    Update CarouselSection.ts

--- sources/local/models/CarouselSection.ts
+++ sources/local/models/CarouselSection.ts
@@ -4,7 +4,7 @@ export const CarouselSection: Model = {
     type: 'object',
     name: 'CarouselSection',
     label: 'Carousel',
-    labelField: 'title.text',
+    labelField: 'title',
     fields: [
         {
             type: 'model',
