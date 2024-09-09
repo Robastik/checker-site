24361bfdafa1b04d35318af2295ac5fb7538d146
Denis Mamaev <expertykt@mail.ru>
2024-09-09 12:09:59 +0900

    msg

--- sources/local/models/CarouselSection.ts
+++ sources/local/models/CarouselSection.ts
@@ -4,7 +4,7 @@ export const CarouselSection: Model = {
     type: 'object',
     name: 'CarouselSection',
     label: 'Carousel',
-    labelField: 'title',
+    labelField: 'title.Text',
     fields: [
         {
             type: 'model',
