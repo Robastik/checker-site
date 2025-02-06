f7317b756659bcc059323df9d557c9ee9efe2321
Denis Mamaev <expertykt@mail.ru>
2025-02-06 12:57:56 +0900

    Update index.tsx

--- src/components/layouts/PostLayout/index.tsx
+++ src/components/layouts/PostLayout/index.tsx
@@ -22,15 +22,7 @@ export default function PostLayout(props) {
                         <header className="max-w-4xl mx-auto mb-12 text-center">
                             <h1 {...(enableAnnotations && { 'data-sb-field-path': 'title' })}>{title}</h1>
                             <div className="text-sm uppercase mt-4">
-                                <!--<time dateTime={dateTimeAttr} {...(enableAnnotations && { 'data-sb-field-path': 'date' })}>
-                                    {formattedDate}
-                                </time>
-                                {author && (
-                                    <>
-                                        <span className="mx-2">|</span>
-                                        <PostAuthor author={author} enableAnnotations={enableAnnotations} />
-                                    </>
-                                )}-->
+                                
                             </div>
                         </header>
                         {markdown_content && (
